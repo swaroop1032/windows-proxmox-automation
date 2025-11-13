6cddc48eb5fe3d1df02f98ded58f4f2e28b49b17
swaroop1032 <161610017+swaroop1032@users.noreply.github.com>
2025-11-13 13:28:29 +0530

    Update main.tf

--- packer/terraform/main.tf
+++ packer/terraform/main.tf
@@ -1,30 +1,8 @@
-provider "proxmox" {
-  pm_api_url = "https://${var.proxmox_host}:8006/api2/json"
-  pm_user    = "root@pam"
-  pm_password = var.proxmox_password
-  pm_tls_insecure = true
+provider "proxmox" { pm_api_url = var.pm_api_url pm_api_token_id = var.pm_api_token_id pm_api_token_secret = var.pm_api_token_secret pm_tls_insecure = true }
+resource "proxmox_vm_qemu" "win" { name = var.vm_name target_node = var.node
+clone = "win11-proxmox-template"
+cores = 4 sockets = 1 memory = 4096
+disk { slot = 0 size = "50G" type = "scsi" storage = var.storage }
+network { model = "virtio" bridge = "vmbr0" }
+ssh_forward_ip = ""
 }
-
-resource "proxmox_vm_qemu" "win11_clone" {
-  name         = "win11-${var.vm_name}"
-  target_node  = "pve"
-  clone        = "win11-golden-template"
-  full_clone   = true
-  cores        = 4
-  memory       = 8192
-
-  disk {
-    size    = "80G"
-    storage = "local-lvm"
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  os_type = "win11"
-  agent   = 1
-  boot    = "cdn"
-}
-

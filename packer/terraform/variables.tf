9a7b1e869c1b319f893b4cb5549ab0ff72acede2
swaroop1032 <161610017+swaroop1032@users.noreply.github.com>
2025-11-13 13:34:30 +0530

    Update variables.tf

--- packer/terraform/variables.tf
+++ packer/terraform/variables.tf
@@ -1,6 +1,6 @@
-variable "pm_api_url" { type = string default = "https://192.168.31.180:8006/api2/json" }
-variable "pm_api_token_id" { type = string }
-variable "pm_api_token_secret" { type = string }
-variable "vm_name" { type = string default = "win11-vm" }
-variable "node" { type = string default = "pve" }
-variable "storage" { type = string default = "local-lvm" }
+variable "pm_api_url"         { default = "https://192.168.31.180:8006/api2/json" }
+variable "pm_api_token_id"    {}
+variable "pm_api_token_secret" {}
+variable "vm_name"            { default = "win11" }
+variable "node"               { default = "pve" }
+variable "storage"            { default = "local-lvm" }

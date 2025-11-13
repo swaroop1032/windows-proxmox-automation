c1ccc052d12094b822d83e10dfcf009053b37a88
swaroop1032 <161610017+swaroop1032@users.noreply.github.com>
2025-11-13 13:28:03 +0530

    Update variables.tf

--- packer/terraform/variables.tf
+++ packer/terraform/variables.tf
@@ -1,5 +1,6 @@
-variable "proxmox_host" {}
-variable "proxmox_password" {}
-variable "vm_name" {
-  default = "auto"
-}
+variable "pm_api_url" { type = string default = "https://192.168.31.180:8006/api2/json" }
+variable "pm_api_token_id" { type = string }
+variable "pm_api_token_secret" { type = string }
+variable "vm_name" { type = string default = "win11-vm" }
+variable "node" { type = string default = "pve" }
+variable "storage" { type = string default = "local-lvm" }
